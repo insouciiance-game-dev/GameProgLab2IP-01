b41ed70b8f33b8b25caddb962344ca2ffdbd02d2
Mykyta Kovalenko <nikkv.ottf@gmail.com>
2023-09-29 09:41:30 +0200

    lab2 player mvmt, 2 scenes, trigger, scripts

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField]
+    private float ySensitivity;
+    
+    [SerializeField]
+    private float xSensitivity;
+
+    [SerializeField]
+    private Transform orientation;
+
+    private float yRotation;
+    private float xRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * xSensitivity;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * ySensitivity;
+
+        yRotation += mouseX;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private int sceneIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
